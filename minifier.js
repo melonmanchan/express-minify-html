243d2fc96f364ade57c731f47e29d5f356183134
Matti Jokitulppo <melonmanchan@gmail.com>
2016-01-10 20:20:51 +0200

    Add option for adding a callback

--- minifier.js
+++ minifier.js
@@ -7,28 +7,41 @@ function minifyHTML(opts) {
 
     function minifier(req, res, next) {
 
-        var sendMinified = function (err, html) {
-            if (err) {
-                console.error(err)
-                if (opts.displayErrors === true) {
-                    res.send('Rendering error: ' + err.message);
-                } else {
-                    res.sendStatus(500);
+        var sendMinified = function (callback) {
+
+            // No callbak specified, just minify and send to client.
+            if (typeof callback === 'undefined') {
+                return function (err, html) {
+                    if (err) {
+                        console.error(err)
+                        if (opts.displayErrors === true) {
+                            res.send('Rendering error: ' + err.message);
+                        } else {
+                            res.sendStatus(500);
+                        }
+                    } else {
+                        html = minify(html, opts.htmlMinifier);
+                        res.send(html);
+                    }
                 }
             } else {
-                html = minify(html, opts.htmlMinifier);
-                res.send(html);
+
+                // Custom callback specified by user, use that one
+                return function (err, html) {
+                    html = minify(html, opts.htmlMinifier);
+                    callback(err, html);
+                }
             }
         };
 
         if (opts.override === false) {
-            res.renderMin = function (view, renderOpts) {
-                this.render(view, renderOpts, sendMinified);
+            res.renderMin = function (view, renderOpts, callback) {
+                this.render(view, renderOpts, sendMinified(callback));
             }
         } else {
             res.oldRender = res.render;
-            res.render = function (view, renderOpts) {
-                this.oldRender(view, renderOpts, sendMinified);
+            res.render = function (view, renderOpts, callback) {
+                this.oldRender(view, renderOpts, sendMinified(callback));
             };
         }
 
