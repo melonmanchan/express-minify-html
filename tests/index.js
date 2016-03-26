c3348972b75110a0800ea69fb555d831113e9d0a
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-26 17:58:31 +0200

    Add handlebars test

--- tests/index.js
+++ tests/index.js
@@ -5,10 +5,18 @@ var request = require('supertest');
 var test    = require('tape');
 
 var minifyHTML = require('../minifier.js');
+var expectedHTML = '<!DOCTYPE html><html><head class=head><meta charset=UTF-8><title>Express minify HTML handlebars example</title></head><body id=body>Hello world</body><script type=text/javascript>function reallyNiceFunction(){return"Hello!"}</script></html>';
+
+var exhbs = require('express-handlebars');
+var hbs  = exhbs.create({
+    defaultLayout: 'test',
+    layoutsDir:    __dirname
+});
 
 var app = express();
 
-app.set('view engine', 'ejs');
+app.engine('handlebars', hbs.engine);
+
 app.set('views', __dirname);
 
 app.use(minifyHTML({
@@ -28,11 +36,25 @@ app.get('/test', function (req, res, next) {
 });
 
 test('Should minify EJS templates', function (t) {
+    app.set('view engine', 'ejs');
+
     request(app)
         .get('/test')
         .expect(200)
         .end(function (err, res) {
-            t.equal(res.text, '<!DOCTYPE html><html><head class=head><meta charset=UTF-8><title>Express minify HTML handlebars example</title></head><body id=body>Hello world</body><script type=text/javascript>function reallyNiceFunction(){return"Hello!"}</script></html>');
+            t.equal(res.text, expectedHTML);
+            t.end();
+        });
+});
+
+test('Should minify Handlebars templates', function (t) {
+    app.set('view engine', 'handlebars');
+
+    request(app)
+        .get('/test')
+        .expect(200)
+        .end(function (err, res) {
+            t.equal(res.text, expectedHTML);
             t.end();
         });
 });
